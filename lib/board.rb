6b1ed219368951fc76836e30c75b13d4099a1eb6
onifernando1 <onifernando1@gmail.com>
2022-08-08 15:11:44 +0100

    Add letters to board, add numbers to board, join board, show board functions

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,10 @@
+require 'colorize'
 # Pseudocode 
 
-#make board 8x8
+#make board 8x8 DONE 
 #show board 
 # way to switch board/flip board
-#reset board to array from joined
+#reset board to array from joined DONE 
 #Give each piece a class: eight pawns, two bishops, two knights, two rooks, one queen, and one king.
 # Arrays for possible moves of pieces
 #legal moves - within board limits 8x8 
@@ -25,16 +26,82 @@ class Board
 
     def initialize 
         make_board()
+        @white_square = "    ".colorize(background: :white)
+        @black_square = "    ".colorize(background: :black)
+        colour_board()
     end 
 
     def make_board
         @board = Array.new(8) { Array.new(8) { "a" } }
     end 
 
+    def colour_board
+
+        odd = [1,3,5,7]
+        even = [0,2,4,6]
+        
+        @board.each_index do |index|
+            if index == 0 || index == 2 || index == 4 || index == 6
+                for i in odd 
+                    @board[index][i] = @black_square
+                end 
+                
+                for i in even 
+                    @board[index][i] = @white_square
+                end 
+            end 
+
+            if index == 1 || index == 3 || index == 5 || index == 7
+                
+                for i in odd 
+                    @board[index][i] = @white_square
+                end 
+
+                for i in even 
+                    @board[index][i] = @black_square
+                end 
+            end 
+        end 
+
+    end 
+
+    def add_numbers_to_board
+        
+        @numbers = [1,2,3,4,5,6,7,8]
+        @numbers.each do |number|
+            @board[number-1].prepend(number.to_s + " ")
+            @board[number-1] << ( + " " + number.to_s)
+        end 
+
+    end 
+
+    def add_letters_to_board
+        
+        @letters = ["   A","   B","   C","   D","   E","   F","   G","   H"]
+        @board.prepend(@letters)
+        @board << @letters
+
+    end 
+
+    def join_board
+
+        @board = @board.map do |array| 
+            array.join("")
+        end 
+
+
+    end 
+
     def show_board
+        
         @temp = @board
-        @board = @board.map{|array| array.join("")}
+        
+        add_numbers_to_board()
+        add_letters_to_board()
+        join_board()
+
         puts @board
+
         @board = @temp
     end 
 
@@ -43,4 +110,4 @@ class Board
 end 
 
 board = Board.new()
-board.make_board
+board.show_board()
