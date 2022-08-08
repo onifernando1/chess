7a48746258f72f5bbce0d52dab9878c27b9f4443
onifernando1 <onifernando1@gmail.com>
2022-08-08 17:37:56 +0100

    Add move function

--- lib/piece.rb
+++ lib/piece.rb
@@ -1,2 +1,16 @@
 class Piece
+
+    def initialize(current_board)
+        @current_board = current_board.board
+
+    end 
+
+    def move(x=3, y=3)
+
+        if @current_board[x][y] == @black_square
+            @current_board[x][y] = @symbol.colorize(background: :light_magenta)
+        elsif @current_board[x][y] == @white_square
+            @current_board[x][y] = @symbol.colorize(background: :light_cyan)
+        end 
+    end 
 end 
