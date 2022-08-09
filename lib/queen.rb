a7140e2a830577bd77cc9a61ad4007e30a79a5dd
onifernando1 <onifernando1@gmail.com>
2022-08-09 15:59:04 +0100

    Queen potential moves

--- lib/queen.rb
+++ lib/queen.rb
@@ -13,6 +13,7 @@ class Queen < Piece
         @string = " \u265B " 
         @symbol =  @string.encode("utf-8").light_white
         @current_position = [0,0]
+        potential_moves()
 
         # @current_board = current_board.board
         # @white_square = "   ".colorize(background: :light_cyan)
@@ -26,5 +27,12 @@ class Queen < Piece
 
     def change_colour()
         super 
+    end 
+
+    def potential_moves
+
+        @potential_x = [+1,-1,0,0,+2,-2,0,0,+3,-3,0,0,+4,-4,0,0,+5,-5,0,0,+6,-6,0,0,+7,-7,0,0,-1,-1,+1,+1,-2,-2,+2,+2,-3,-3,+3,+3,-4,-4,+4,+4,-5,-5,+5,+5,-6,-6,+6,+6,-7,-7,+7,+7]
+        @potential_y = [0,0,-1,+1,0,0,-2,+2,0,0,-3,+3,0,0,-4,+4,0,0,-5,+5,0,0,-6,+6,0,0,-7,+7,+1,-1,+1,-1,+2,-2,+2,-2,+3,-3,+3,-3,+4,-4,+4,-4,+5,-5,+5,-5,+6,-6,+6,-6,+7,-7,+7,-7]
+
     end 
 end 
