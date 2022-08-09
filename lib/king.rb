643bb820157a3433c2eb0ffc6cbcb8c527bd0d67
onifernando1 <onifernando1@gmail.com>
2022-08-09 16:01:24 +0100

    King potential moves

--- lib/king.rb
+++ lib/king.rb
@@ -6,12 +6,13 @@ require 'colorize'
 class King < Piece
 
     attr_accessor :current_position
-    
+
     def initialize(current_board, colour="white")
         super 
         @string = " \u265A " 
         @symbol =  @string.encode("utf-8").light_white
         @current_position = [0,0]
+        potential_moves()
         # @current_board = current_board.board
         # @white_square = "   ".colorize(background: :light_cyan)
         # @black_square = "   ".colorize(background: :light_magenta)
@@ -24,6 +25,13 @@ class King < Piece
 
     def change_colour()
         super 
+    end 
+
+    def potential_moves
+       
+        @potential_x = [+1,-1,0,0,-1,-1,+1,+1]
+        @potential_y = [0,0,-1,+1,+1,-1,+1,-1]
+    
     end 
    
 end 
