f24f0b62f6d313a0183ddc43f1326e83914e890e
onifernando1 <onifernando1@gmail.com>
2022-08-20 01:11:40 +0100

    main

--- lib/main.rb
+++ lib/main.rb
@@ -1,10 +1,7 @@
 # frozen_string_literal: true
 
-require_relative 'bishop'
-require_relative 'board'
-require_relative 'knight'
-require_relative 'pawn'
-require_relative 'piece'
-require_relative 'queen'
-require_relative 'king'
-require_relative 'rook'
+require_relative 'game'
+
+game = Game.new
+game.game
+game.win_message
